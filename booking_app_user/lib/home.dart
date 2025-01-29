5fd6bf29b166381c2dfe4dcc222928cb6cd53285
Pedro Paradela <pedrojunqueira5@gmail.com>
2025-01-29 10:51:48 -0300

    Menu do usuario (minhas propriedades, deslogar e login)

--- booking_app_user/lib/home.dart
+++ booking_app_user/lib/home.dart
@@ -17,13 +17,13 @@ class _HomeState extends State<Home> {
   List<Propriedade> propriedades = [];
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
-  TextEditingController numberController =  TextEditingController();
+  TextEditingController numberController = TextEditingController();
   TextEditingController complementController = TextEditingController();
   TextEditingController priceController = TextEditingController();
   TextEditingController maxguestController = TextEditingController();
   TextEditingController cepController = TextEditingController();
   File? _image;
-//TODO: falta ver a questao da imagem, deletar a propriedade e editar ela
+
   Card criarCard(Propriedade propriedade) {
     return Card(
       elevation: 4.0,
@@ -94,16 +94,14 @@ class _HomeState extends State<Home> {
 
   Future<void> pickImage() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles(
-      type: FileType.image, // Permite selecionar apenas imagens
+      type: FileType.image,
     );
 
     if (result != null) {
       PlatformFile file = result.files.first;
       _image = File(file.path!);
       print("Imagem selecionada: ${_image!.path}");
-
     }
-
   }
 
   @override
@@ -114,20 +112,61 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Home Page'),
-        ),
-        body: Column(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: propriedades.length,
-                itemBuilder: (context, index) {
-                  return criarCard(propriedades[index]);
-                },
+            const Text('Propriedades'),
+            PopupMenuButton<String>(
+              icon: const CircleAvatar(
+                child: Icon(Icons.person),
               ),
+              onSelected: (String result) {
+                if (result == 'minhas_propriedades') {
+
+                } else {
+                  Navigator.pop(context);
+                }
+              },
+              itemBuilder: (BuildContext context) {
+                if (widget.usuario.id != -1) {
+                  // Usuário está logado
+                  return <PopupMenuEntry<String>>[
+                    const PopupMenuItem<String>(
+                      value: 'minhas_propriedades',
+                      child: Text('Minhas Propriedades'),
+                    ),
+                    const PopupMenuItem<String>(
+                      value: 'deslogar',
+                      child: Text('Deslogar'),
+                    ),
+                  ];
+                } else {
+                  // Usuário não está logado
+                  return <PopupMenuEntry<String>>[
+                    const PopupMenuItem<String>(
+                      value: 'login',
+                      child: Text('Fazer Login'),
+                    ),
+                  ];
+                }
+              },
             ),
           ],
-        ));
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: propriedades.length,
+              itemBuilder: (context, index) {
+                return criarCard(propriedades[index]);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
